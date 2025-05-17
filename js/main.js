ab305e833c474207e03845f724a9fa553751c48a
AazadAhmad <40417095+AazadAhmad@users.noreply.github.com>
2025-05-18 01:11:09 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,15 +16,66 @@ document.addEventListener("click", function (event) {
 
 $(document).ready(function () {
   var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
+    cssMode: false,
+    loop: false,
+    autoplay: false,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
-    mousewheel: true,
+    mousewheel: false,
     keyboard: true,
+  });
+
+  $(".heading svg").on("click", function () {
+    $(this).toggleClass("active");
+  });
+
+  // $(".navbar-toggler-icon").on("click", function () {
+  //   $("body").addClass("body-overlay");
+  // });
+  $(".close-menu").on("click", function () {
+    $(".navbar-collapse").removeClass("show");
+    // $("body").removeClass("body-overlay");
+  });
+
+  const $input = $("#searchInput");
+  const $closeBtn = $(".close-btn");
+
+  $input.on("input", function () {
+    if ($(this).val().length > 0) {
+      $closeBtn.show();
+    } else {
+      $closeBtn.hide();
+    }
+  });
+
+  $closeBtn.on("click", function () {
+    $input.val("");
+    $closeBtn.hide();
+  });
+
+  $(".pop-btn").on("click", function () {
+    $(".mobile-filter").toggleClass("show-filter");
+  });
+  $(".close_btn").on("click", function () {
+    $(".mobile-filter").removeClass("show-filter");
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const mapSection = document.querySelector(".map-s");
+    const offsetTop = 150;
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > offsetTop) {
+        mapSection.classList.add("sticky");
+      } else {
+        mapSection.classList.remove("sticky");
+      }
+    });
   });
 });
